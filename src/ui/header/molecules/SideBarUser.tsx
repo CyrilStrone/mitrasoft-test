9581554e1bb4768f9dfc372a834023100696b3d2
CyrilStrone <cyrilstrone@gmail.com>
2023-05-28 19:25:31 +0700

    Side Bar
    Change SideBar

--- src/ui/header/molecules/SideBarUser.tsx
+++ src/ui/header/molecules/SideBarUser.tsx
@@ -1,15 +1,18 @@
 import "../styles/SideBarUser.css";
+import Avatar from '../../../assets/listOfPosts/avatar.jpg'
 
 
 export const SideBarUser = () => {
     return (
         <div className="SideBarUser">
-            <img className='SideBarUser__Avatar' src="" alt="" />
-            <div className='SideBarUser__Name'>
-                Name
-            </div>
-            <div className='SideBarUser__Mail'>
-                Mail
+            <img className='SideBarUser__Avatar' src={Avatar} alt="" />
+            <div className="SideBarUser__UserInfo">
+                <div className='SideBarUser__UserInfo__Name'>
+                    Name
+                </div>
+                <div className='SideBarUser__UserInfo__Mail'>
+                    Mail
+                </div>
             </div>
         </div>
     );
